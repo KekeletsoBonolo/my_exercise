55c1684ac830aefb42060ac85155a76bb50fb777
KekeletsoBonolo <127309506+KekeletsoBonolo@users.noreply.github.com>
2023-03-14 22:24:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from math import sqrt
+#homework 1
+print("Homework 2")
+x = 2
+y = 4
+
+h = sqrt( x ** 2 + y ** 2 )
+
+print(h)
+
+#homework 2
+print(" ")
+print("Homework 2")
+
+y = 2
+h = 3
+
+x = sqrt( h ** 2 - y ** 2)
+
+print(x)
